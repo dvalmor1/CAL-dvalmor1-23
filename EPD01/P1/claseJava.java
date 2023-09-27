deaf0830a4376d7ac36aa0e489efc2ee684d1e34
dvalmor1 <dvalmor1@alu.upo.es>
2023-09-27 19:06:48 +0200

    Agregar main

--- EPD01/P1/claseJava.java
+++ EPD01/P1/claseJava.java
@@ -1,5 +1,6 @@
 public class claseJava {
 	public static void main(String[] args) {
 		System.out.println("Hola, clase de EPD 1");
+		System.out.println("Esto ha sido hecho en una clase de github");
 	}
 }
